2bff38acd49244439a36011104f5a2e2b631d626
JerryU <jerry.uusitalo@gmail.com>
2024-01-28 22:08:01 +0200

    First commit

---
+++ src/__tests__/sampletest.test.tsx
@@ -0,0 +1,5 @@
+describe("First test group", () => {
+    test("Test if true is true", () => {
+        expect(true).toBe(true)
+    })
+})
---
+++ src/components/TestingComponent.tsx
@@ -0,0 +1,10 @@
+const TestingComponent = () => {
+    return (
+        <>
+        <h1>This component is used for testing</h1>
+        <p>Text content</p>
+        </>
+    )
+}
+
+export default TestingComponent
